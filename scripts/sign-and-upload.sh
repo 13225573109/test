34d7b49eeee79257976d0c59cb425485913d52a4
13225573109 <13225573109@126.com>
2016-05-19 16:19:55 +0800

    gogogo

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -11,3 +11,13 @@ PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/AdHocWild
 INPUTDIR="$PWD/build/Release-iphoneos"
 OUTPUTDIR="$PWD/build"
 xcrun -log -sdk iphoneos PackageApplication -v "$INPUTDIR/test.app" -o "/$OUTPUTDIR/KuaiYouJia.ipa"
+if [ ! -z "f4f125aac99b0777c58c1e9d4e3e6138" ] && [ ! -z "244bb8a3f9cea45b9aaa322339f7910f" ]; then
+echo ""
+echo "***************************"
+echo "* Uploading to pgyer  *"
+echo "***************************"
+curl http://www.pgyer.com/apiv1/app/upload \
+-F uKey="f4f125aac99b0777c58c1e9d4e3e6138" \
+-F _api_key="244bb8a3f9cea45b9aaa322339f7910f" \
+-F file="@$OUTPUTDIR/$APP_NAME.ipa"
+fi
