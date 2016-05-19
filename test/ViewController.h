2a3339ae3463bf9fb9dafc04ab146b03eb9bea42
13225573109 <13225573109@126.com>
2016-05-19 14:48:26 +0800

    7

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by yan on 16/5/18.
+//  Copyright © 2016年 xujie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by yan on 16/5/18.
+//  Copyright © 2016年 xujie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIButton *testBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    testBtn.frame = CGRectMake(100, 100, 100, 100);
+    testBtn.backgroundColor = [UIColor redColor];
+    [testBtn setTitle:@"测试" forState:UIControlStateNormal];
+    [testBtn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:testBtn];
+}
+
+-(void)btnClick
+{
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"测试" message:@"测试" delegate:nil cancelButtonTitle:@"test" otherButtonTitles:nil, nil];
+    [alert show];
+}
+
+@end
