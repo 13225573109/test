056159e239ea901cd56f6c7dd46e7668230abc61
13225573109 <13225573109@126.com>
2016-05-19 16:22:57 +0800

    last

--- scripts/sign-and-upload.sh
+++ scripts/sign-and-upload.sh
@@ -10,7 +10,7 @@ fi
 PROVISIONING_PROFILE="$HOME/Library/MobileDevice/Provisioning Profiles/AdHocWildcard.mobileprovision"
 INPUTDIR="$PWD/build/Release-iphoneos"
 OUTPUTDIR="$PWD/build"
-xcrun -log -sdk iphoneos PackageApplication -v "$INPUTDIR/test.app" -o "/$OUTPUTDIR/KuaiYouJia.ipa"
+xcrun -log -sdk iphoneos PackageApplication -v "$INPUTDIR/test.app" -o "$OUTPUTDIR/KuaiYouJia.ipa"
 if [ ! -z "f4f125aac99b0777c58c1e9d4e3e6138" ] && [ ! -z "244bb8a3f9cea45b9aaa322339f7910f" ]; then
 echo ""
 echo "***************************"
@@ -19,5 +19,5 @@ echo "***************************"
 curl http://www.pgyer.com/apiv1/app/upload \
 -F uKey="f4f125aac99b0777c58c1e9d4e3e6138" \
 -F _api_key="244bb8a3f9cea45b9aaa322339f7910f" \
--F file="@$OUTPUTDIR/$APP_NAME.ipa"
+-F file="$OUTPUTDIR/$APP_NAME.ipa"
 fi
