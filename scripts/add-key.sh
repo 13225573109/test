e540662927e34c8e88a043f0b94b5900e612c958
13225573109 <13225573109@126.com>
2016-05-19 15:53:59 +0800

    add_key

--- scripts/add-key.sh
+++ scripts/add-key.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 security create-keychain -p travis ios-build.keychain
+security default-keychain -s ios-build.keychain
+security unlock-keychain -p travis ios-build.keychain
+security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 security import ./scripts/certs/apple.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P qwer1234 -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./scripts/profile/AdHocWildcard.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+security find-identity -p codesigning ~/Library/Keychains/ios-build.keychain
